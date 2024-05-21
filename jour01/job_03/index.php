a2f0a6f48aaa089d5e4b0483224638cd4ed0d5fc
Derroce <162974272+Derroce@users.noreply.github.com>
2024-05-21 16:05:33 +0200

    tableau

---
+++ jour01/job_03/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tableau avec 3 colonnes</title>
+</head>
+<body>
+    <?php
+            // Définir les données du tableau
+            
+            ?>
+            <table border="2">
+                <tr>
+                    <th>Type</th>
+                    <th>Nom</th>
+                    <th>Valeur</th>
+                </tr>
+            
+            
+                <tr>
+                    <td>Boolean</td>
+                    <td>Variable 1</td>
+                    <td>true</td>
+                </tr>   
+                <tr>
+                    <td>String</td>
+                    <td>Variable 2</td>
+                    <td>"Hello"</td>
+                </tr>   
+                <tr>
+                    <td>Integer</td>
+                    <td>Variable 3</td>
+                    <td>65</td>
+                </tr>
+            
+                <tr>
+                    <td>Float</td>
+                    <td>Variable 4</td>
+                    <td>43.7</td>
+                </tr>   
+            </table>
+
+</body>
+</html>
