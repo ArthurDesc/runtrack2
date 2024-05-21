3f17ae97f92613a821d70d83e676c7713a436da4
Derroce <162974272+Derroce@users.noreply.github.com>
2024-05-21 14:50:10 +0200

    ajout commentaires

---
+++ jour01/job_02/index.php
@@ -0,0 +1,12 @@
+<?php
+$phrase = "Hello LaPlateforme";
+/*
+Ceci est un commentaire
+sur plusieurs lignes
+*/
+echo str_repeat($phrase, 2); // Ceci est un commentaire
+
+# commentaires courts
+
+
+?>
