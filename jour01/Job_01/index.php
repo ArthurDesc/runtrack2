002242911cc5d32882e7c07f7f19b2658e2194be
Derroce <162974272+Derroce@users.noreply.github.com>
2024-05-21 14:36:09 +0200

    ended exercise

---
+++ jour01/Job_01/index.php
@@ -0,0 +1,35 @@
+<?php
+
+    $str = 'LaPlateforme';
+
+    
+    $str2 = 'Vive';
+    
+    $str3 = '!';
+
+    $phrase = $str2. ' '. $str. $str3; 
+    
+        echo $phrase . "<br>"; 
+
+
+
+
+    $val = 6;
+        echo $val . "<br>";
+        echo $val+4 . "<br>";
+    ?>
+
+<?php
+
+    $myBool = true;
+        echo $myBool;
+
+    $myBool = false;
+        echo $myBool;
+
+    
+
+
+
+?>
+
