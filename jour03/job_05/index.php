c8c97ef51defc7fdb43080881f595bf4d9f84d6a
Derroce <162974272+Derroce@users.noreply.github.com>
2024-05-23 21:39:57 +0200

    jobe 06

--- jour03/job_05/index.php
+++ jour03/job_05/index.php
@@ -6,14 +6,30 @@
 </head>
 <body>
     <?php
-        $str = "On n'est pas le meilleur quand on le croit mais quand on le sait."
-        $dict = array (
-            "voyelles" => array ("a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y");
+        $str = "On n'est pas le meilleur quand on le croit mais quand on le sait.";
+        $dict = array(
+            "voyelles" => array ("a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y"),
             "consonnes" => array ("b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z")
         );
+
+        $compteur = array(
+            "voyelles" => 0,
+            "consonnes" => 0
+        );
+
+        for ($index=0; isset($str[$index]);$index++) {
+                if (in_array($str[$index], $dict["voyelles"])) { 
+                    $compteur ["voyelles"]++;
+            
+                } elseif (in_array($str[$index], $dict["consonnes"])) {
+                    $compteur ["consonnes"]++;
+                }
+            }
+
+
     ?>
 
-    <table border="2">
+    <table border ="2">
 
         <tr>
             <th>Consonnes</th>
@@ -21,8 +37,8 @@
         </tr>
 
         <tr>
-            <td><?php ?></td>
-            <td><?php ?></td>
+            <td><?php echo $compteur["consonnes"]?></td>
+            <td><?php echo $compteur["voyelles"]?></td>
         </tr>
 
 
