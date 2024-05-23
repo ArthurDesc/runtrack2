03bcc495896432f63e826e3e520fbdfc3390dc0f
Derroce <162974272+Derroce@users.noreply.github.com>
2024-05-23 16:58:18 +0200

    job_05

---
+++ jour03/job_05/index.php
@@ -0,0 +1,33 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $str = "On n'est pas le meilleur quand on le croit mais quand on le sait."
+        $dict = array (
+            "voyelles" => array ("a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y");
+            "consonnes" => array ("b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z")
+        );
+    ?>
+
+    <table border="2">
+
+        <tr>
+            <th>Consonnes</th>
+            <th>Voyelles</th>
+        </tr>
+
+        <tr>
+            <td><?php ?></td>
+            <td><?php ?></td>
+        </tr>
+
+
+    </table>
+
+
+</body>
+</html>
