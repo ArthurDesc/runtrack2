9ae076cc588d92b199f7fec8c903af3e21f92341
Derroce <162974272+Derroce@users.noreply.github.com>
2024-05-24 10:50:10 +0200

    job 7 ended

--- jour03/job_07/index.php
+++ jour03/job_07/index.php
@@ -8,14 +8,13 @@
     <?php
         $str = "Certaines choses changent, et d'autres ne changeront jamais.";
         $temp = $str[0];
+        $phrase_mixed = "";
 
 
         for ($index=1; isset($str[$index]); $index++) {
-            for ($index= 0; $index >1; $index-- ) {
                 $phrase_mixed .= $str[$index];
             }
 
-        }
 
         $phrase_mixed .= $temp;
 
