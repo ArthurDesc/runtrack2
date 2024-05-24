e6a4a1239039097429c2ea928e8e0d1a3504943d
Derroce <162974272+Derroce@users.noreply.github.com>
2024-05-24 16:56:08 +0200

    début de job 6

---
+++ jour04/job_06/index.php
@@ -0,0 +1,38 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form name="index.php" method="get">
+
+
+<label for="nombre">Nombre : </label>
+<input type="text" id="nombre" name="nombre">
+
+<input type="submit" value="Envoyer">
+
+</form>
+    <?php
+
+    $nombre = $_GET['nombre'];
+
+    if ($_SERVER ['REQUEST_METHOD'] == 'GET' && isset($nombre)) {
+        foreach ($_GET as $key => $value) {
+    if ($_ == 2 % 0) {
+    echo "Nombre pair";
+
+    } else { 
+        echo "nombre impair";
+    }
+}
+    }
+
+    
+
+    ?>
+    
+</body>
+</html>
