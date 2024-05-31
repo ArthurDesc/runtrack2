8fd625a121b8f8103c1af7277bb9786599434a04
Derroce <162974272+Derroce@users.noreply.github.com>
2024-05-31 16:53:17 +0200

    job 03

--- jour08/job_03/index.php
+++ jour08/job_03/index.php
@@ -1,53 +1,37 @@
-<?php
-session_start(); // Démarre la session
-// Initialisation de la variable de session "prenom" si elle n'est pas déjà définie
-if (!isset($_SESSION["prenoms"])) {
-    $_SESSION["prenoms"] = array();
+<?php 
+
+session_start();
+
+//if session is null, create a new one
+if(!isset($_SESSION['prenoms'])){
+    $_SESSION['prenoms'] = [];
+}
+//if add isn't null and the text bar isn't null, ge the post form and specify who is it with sessions
+if(isset($_POST['add']) && isset($_POST['fname'])){
+    $prenom = $_POST['fname'];
+    $_SESSION['prenoms'][] = $prenom;
 }
 
-// Traitement des requêtes POST
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Réinitialisation du prénom si le bouton "Reset" est cliqué
-    if (isset($_POST["reset"])) {
-        $_SESSION["prenoms"] = array();
-    }
-
-    // Enregistrement du prénom dans la session si le champ "prenom" est soumis
-    elseif (isset($_POST["prenom"]) && !empty($_POST["prenom"])) {
-        $_SESSION["prenoms"][] = $_POST["prenom"];
-    }
+//reset the session
+if(isset($_POST['reset'])){
+    $_SESSION['prenoms'] = [];
 }
+
+//if session isn't empty iterate on sessions "prenoms" and echo it
+if(!empty($_SESSION['prenoms'])){
+  foreach($_SESSION['prenoms'] as $name){
+    echo "<ul><li>$name</li></ul>" . "<br/>";
+  }
+}
+
+
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-</head>
-<body>
 
 <form action="index.php" method="post">
-    <input type="text" name="prenom" >
-    <input type="submit" name="submit" value="Envoyer">
-    <br />
-
-    <input type="submit" name="reset" value="Reset">
+  <label for="fname">Username</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <input type="submit" name="add" value="Add">
+  <label for="fname">Reset</label>
+  <input type="submit" name="reset" value="Reset">
 </form>
-
-<?php
-// Affichage du prénom si il est défini dans la session
-if (!empty($_SESSION["prenoms"])) {
-    echo "<ul>";
-    foreach ($_SESSION["prenoms"] as $prenom) {
-        echo "<li>". htmlspecialchars($prenom) ."</li>";
-    }
-    echo "</ul>";
-}
-?>
-
-</body>
-</html>
